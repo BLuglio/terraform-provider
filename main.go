a3b44e725347dd3314991a037e35534cdb117a6f
BLuglio <biagio.luglio@gmail.com>
2023-04-04 23:08:40 +0200

    fix

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/BLuglio/terraform-provider/custom_provider"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: cust.Provider,
+		ProviderFunc: custom_provider.Provider,
 	})
 }
